de2689b9f28f17af10a3a9d14b3deabb4bf33d6a
Никита <oleinik.nikita96@gmail.com>
2022-06-26 22:19:21 +0300

    Неполное ДЗ по тестам.

---
+++ src/test/java/ru/hogwarts/school/SchoolApplicationWithMockTests.java
@@ -0,0 +1,62 @@
+package ru.hogwarts.school;
+
+import org.json.JSONObject;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.mock.mockito.SpyBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import ru.hogwarts.school.controller.FacultyController;
+import ru.hogwarts.school.model.Faculty;
+import ru.hogwarts.school.repository.FacultyRepository;
+import ru.hogwarts.school.service.FacultyService;
+
+import java.util.Optional;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest
+@AutoConfigureMockMvc
+public class SchoolApplicationWithMockTests {
+    @Autowired
+    private MockMvc mockMvc;
+    @MockBean
+    private FacultyRepository facultyRepository;
+    @SpyBean
+    private FacultyService facultyService;
+    @InjectMocks
+    private FacultyController facultyController;
+
+    @Test
+    public void saveFacultyTest() throws Exception {
+        Long id = 1L;
+        String name = "NewFaculty";
+
+        JSONObject facultyObject = new JSONObject();
+        facultyObject.put("name", name);
+
+        Faculty faculty = new Faculty();
+        faculty.setId(id);
+        faculty.setName(name);
+
+        Mockito.when(facultyRepository.save(any(Faculty.class))).thenReturn(faculty);
+        Mockito.when(facultyRepository.findById(any(Long.class))).thenReturn(Optional.of(faculty));
+
+        mockMvc.perform(MockMvcRequestBuilders
+                        .post("/faculty") //send
+                        .content(facultyObject.toString())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk()) //receive
+                .andExpect(jsonPath("$.id").value(id))
+                .andExpect(jsonPath("$.name").value(name));
+    }
+}
